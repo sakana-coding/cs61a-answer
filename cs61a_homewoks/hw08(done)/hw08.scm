8f73ba1dd744632356d8165ccec834d13c953f4b
sakana-coding <xuxiang200611@outlook.com>
2025-11-12 10:17:10 +0800

    first commit

---
+++ cs61a_homewoks/hw08(done)/hw08.scm
@@ -0,0 +1,25 @@
+(define (ascending? s)
+  (cond ((null? s) #t)
+    ((null? (cdr s)) #t)
+    ((<= (car s) (car (cdr s)))
+      (ascending? (cdr s)))
+    (else #f)))
+
+(define (my-filter pred s)
+  (cond ((null? s) '())
+    ((pred (car s))
+      (cons (car s) (my-filter pred (cdr s))))
+    (else
+      (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2)
+  (cond
+    ((null? lst1) lst2)
+    ((null? lst2) lst1)
+    (else
+      (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
+
+(define (no-repeats s)
+  (if (null? s) s
+    (cons (car s)
+      (no-repeats (filter (lambda (x) (not (= (car s) x))) (cdr s))))))
---
+++ cs61a_labs/lab09(done)/lab09.scm
@@ -0,0 +1,30 @@
+(define (over-or-under num1 num2) (cond ((> num1 num2) 1)
+    ((= num1 num2) 0)
+    ((< num1 num2) -1)))
+
+(define (make-adder num) (lambda (inc) (+ num inc)))
+
+(define (composed f g) (lambda (x) (f (g x))))
+
+(define (repeat f n)
+  (lambda (x)
+    (define (iter count result)
+      (if (= count n)
+        result
+        (iter (+ count 1) (f result))))
+    (iter 0 x)))
+
+(define (max a b)
+  (if (> a b)
+    a
+    b))
+
+(define (min a b)
+  (if (> a b)
+    b
+    a))
+
+(define (gcd a b)
+  (if (zero? (modulo (max a b) (min a b)))
+    (min a b)
+    (gcd (min a b) (modulo (max a b) (min a b)))))
